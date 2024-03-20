ca8670770f72235ce4aeaafd93c355fa159819e6
Shawn Witter <shawn.witter.gdp@gmail.com>
2024-03-20 01:09:38 -0400

    working with slash commands and embeds

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {Client, IntentsBitField} = require('discord.js');
+const {Client, IntentsBitField, EmbedBuilder} = require('discord.js');
 
 const client = new Client({intents: [
     IntentsBitField.Flags.Guilds,
@@ -26,6 +26,45 @@ client.on('messageCreate', (message)=> {
     if(message.content.toLowerCase() === 'yorgy')
     {
         message.reply('shmorgy!');
+    }
+})
+
+client.on('interactionCreate', (interaction) => {
+    if(!interaction.isChatInputCommand()) return;
+    
+    if(interaction.commandName.toLowerCase() === 'hey')
+    {
+        interaction.reply('hello!')
+    }
+
+    if(interaction.commandName.toLowerCase() === 'ping')
+    {
+        interaction.reply('pong!')
+    }
+
+    if(interaction.commandName.toLowerCase() === 'add')
+    {
+        const num1 = interaction.options.get('first-number').value;
+        const num2 = interaction.options.get('second-number').value;
+        const sum = num1 + num2;
+        interaction.reply(`${num1} + ${num2} = ${sum}`);
+    }
+
+    if(interaction.commandName.toLowerCase() === 'embed')
+    {
+        const embed = new EmbedBuilder()
+        .setTitle('Embed Title')
+        .setDescription('This is an embed desc')
+        .setColor('Blue')
+        .addFields(
+            {name: 'Field Title 1', value: 'Some 1st Random Value', inline: true}, 
+            {name: 'Field Title 2', value: 'Some 2nd Random Value', inline: true}
+            );
+        interaction.reply({embeds: [embed]});
+        // this is used to send a message without replying. causes error on command side with this.
+        // use with message for now
+        // interaction.channel.send({embeds: [embed]});
+
     }
 })
 
